2710b8f70fa02da6600202bdc56c2f9181214a29
kirankv2536 <kirankv2536@gmail.com>
2017-03-24 21:39:49 +0530

    Update services.js

--- assets/js/services/services.js
+++ assets/js/services/services.js
@@ -2,7 +2,7 @@ angular.module('formApp.services', [])
     .service('urls', function() {
 
         this.apiUrl = "https://randomform.herokuapp.com/";
-        this.apiHref = "http://localhost/FormApp/#/";
+        this.apiHref = "http://localhost/randomformapp/#/";
         
         
     })
