b716da3f2d74253f33a55812bd712251801667fe
kirankv2536 <kirankv2536@gmail.com>
2017-03-24 21:30:45 +0530

    Create services.js

---
+++ assets/js/services/services.js
@@ -0,0 +1,9 @@
+angular.module('formApp.services', [])
+    .service('urls', function() {
+
+        this.apiUrl = "https://randomform.herokuapp.com/";
+        this.apiHref = "http://localhost/FormApp/#/";
+        
+        
+    })
+
